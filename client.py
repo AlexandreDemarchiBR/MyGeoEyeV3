6b1ed7bc624a561af178dc3ab7e45226c2ca798c
Milton Alexandre Souza Demarchi <alexandre.demarchi1@gmail.com>
2024-11-11 08:51:18 -0300

    Codigo de datanode e client comentados

--- client.py
+++ client.py
@@ -7,6 +7,10 @@ import threading
 CHUNK_SIZE = 10*1024*1024
 
 class Client:
+    '''
+    Usando nameserver, nos conectamos à main, usamos a gambiarra da chamada remota pra garantir
+    a conexão.
+    '''
     def __init__(self) -> None:
         self.ns = Pyro5.api.locate_ns()
         self.main = Pyro5.api.Proxy("PYRONAME:mainserver")
@@ -15,7 +19,10 @@ class Client:
         if not os.path.exists('client_dir/'):
             os.makedirs('client_dir/')
 
-
+    '''
+    Usando upload_image_socket, pedimos um socket para persistir arquivo de nome file_name,
+    usamos a porta retornada para conectar e enviar os dados.
+    '''
     def send_image(self, file_path):
         file_name = os.path.basename(file_path)
         port = self.main.upload_image_socket(file_name)###############
@@ -23,6 +30,9 @@ class Client:
             s.connect((self.main_ip, port))
             s.sendfile(f)
     
+    '''
+    Semelhante à anterior, mas para download
+    '''
     def download_image(self, file_name):
         file_path = f'client_dir/downloaded_{file_name}'
         port = self.main.download_image_socket(file_name) ############
--- datanode.py
+++ datanode.py
@@ -7,6 +7,12 @@ import threading
 CHUNK_SIZE = 10*1024*1024
 DATANODE_INSTANCE = 1
 
+
+'''
+Função chamada pelo distribute_image da main. Pega um socket que se conecta com o
+cliente (que neste contexto é o main) recebe dados e persiste com o nome passado no
+parametro file_name
+'''
 def store_image(file_name: str, srv_socket: socket.socket):
     srv_socket.listen(1)
     print('aceitando conexão')
@@ -22,7 +28,10 @@ def store_image(file_name: str, srv_socket: socket.socket):
     print('encerrando socket server')
     srv_socket.close()
 
-
+'''
+Semelhante à anterior, mas lê o arquivo de nome file_name, e envia para o socket do
+cliente (que aqui é o main)
+'''
 def retrieve_image(file_name: str, srv_socket: socket.socket):
     srv_socket.listen(1)
     print('aceitando conexão')
@@ -37,6 +46,13 @@ def retrieve_image(file_name: str, srv_socket: socket.socket):
 
 @Pyro5.api.expose
 class Datanode(object):
+    '''
+    O servidor nesta fase de desenvolvimente apaga todos os arquivos, para simplificar a
+    depuração, diminuindo comportamentos imprevisiveis por causa de efeitos colaterais.
+    Futuramente a ideia é listar os arquivos para que a main faça o mapeamento das partes
+    de arquivos de forma dinâmica para aruqivos existentes nos nós.
+    file_locks é um dicionario para locks individuais para arquivos
+    '''
     def __init__(self) -> None:
         self.clean_start()
         if not os.path.exists('datanode_dir/'):
@@ -44,21 +60,27 @@ class Datanode(object):
         self.files = os.listdir('datanode_dir/')
         self.file_locks = {}
 
+    # limpa arquivos persistidos
     def clean_start(self):
         if os.path.exists('datanode_dir/'):
             files = os.listdir('datanode_dir/')
-            print(files)
             for file in files:
                 os.remove(f'datanode_dir/{file}')
+
 
     def get_file_lock(self, file_name):
         if file_name not in self.file_locks:
             self.file_locks[file_name] = threading.Lock()
         return self.file_locks[file_name]
 
+    # usada na gambiarra da main
     def hello(self):
         return "hello world"
 
+    '''
+    Cria socket para main enviar arquivo, delega esse socket pra thread que roda store_image
+    e retorna a porta para a main poder se conectar
+    '''
     def store_image_socket(self, image_name):
         print('criando socket')
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -68,7 +90,9 @@ class Datanode(object):
         print('disparando thread')
         t.start()
         return port
-
+    '''
+    Semelhante à anterior, mas pra recuperar arquivo
+    '''
     def retrieve_image_socket(self, image_name):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(('0.0.0.0', 0))
